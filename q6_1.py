865fa464e3328c3c60bb24618244eaa19d1c65ac
lim <linhujiang@icloud.com>
2015-12-08 19:54:31 +0000

    q6

---
+++ q6_1.py
@@ -0,0 +1,112 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+from pymongo import MongoClient
+import pymongo
+from bson.code import Code
+
+
+client = MongoClient()
+
+#for -- data
+db = client.limdata
+col = db.blog
+
+
+total_num = col.count()
+print "total_num: ",total_num
+
+
+
+col.update({'mr_status':{'$exists':True}},{'$set':{'mr_status':"inprocess"}},False,True);
+
+mapper = Code("""
+	              function() {
+        uniqueWords = function (words){
+            var arrWords = words.split(" ");
+            var arrNewWords = [];
+            var seenWords = {};
+            var arrNewWords2 = [];
+            for(var i=0;i<arrWords.length;i++) {
+                if (!seenWords[arrWords[i]]) {
+                    seenWords[arrWords[i]]=true;
+                    arrNewWords.push(arrWords[i]);
+                }
+            }
+
+            return arrNewWords;
+        }
+      var unigrams =  uniqueWords(this.text) ;
+      emit(this.id_member, {unigrams:unigrams});
+};
+
+	              """)
+
+reducer = Code("""
+	               function(key,values){
+
+    Array.prototype.uniqueMerge = function( a ) {
+        for ( var nonDuplicates = [], i = 0, l = a.length; i<l; ++i ) {
+            if ( this.indexOf( a[i] ) === -1 ) {
+                nonDuplicates.push( a[i] );
+            }
+        }
+        return this.concat( nonDuplicates )
+    };
+
+    unigrams = [];
+    values.forEach(function(i){
+        unigrams = unigrams.uniqueMerge(i.unigrams);
+    });
+    return { unigrams:unigrams};
+};
+	                """)
+
+result = col.map_reduce(mapper, reducer, "unigram_result",query = {'mr_status':"inprocess",'text':{'$type':2}})
+
+
+col.update({'mr_status':"inprocess"},{'$set':{'mr_status':"processed"}},False,True);
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^step 2 : count^^^^^^^^^^^^^^^^^#
+
+col = db.unigram_result
+
+mapper = Code("""
+                  function () {
+
+                    this.value.unigrams.forEach(function(z) {
+                      emit(z, 1);
+                    });
+
+                  }
+                  """)
+
+reducer = Code("""
+                   function (key, values) {
+
+                      var total = 0;
+                      for (var i = 0; i < values.length; i++) {
+                       total += values[i];
+                      }
+                      return total;
+
+                   }
+                    """)
+
+result = col.map_reduce(mapper, reducer, "unigram_result_count")
+
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^step 3 : sort ^^^^^^^^^^^^^^^^^#
+
+col = db.unigram_result_count
+
+col.create_index([('value',pymongo.ASCENDING)])
+data = col.find().sort('value',pymongo.DESCENDING).limit(11)
+
+
+for item in data:
+    print item
+
+
